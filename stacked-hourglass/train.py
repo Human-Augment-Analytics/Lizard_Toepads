f4a37b462c6611606be7eda93a8dd130e5c9f6cd
Albert Xu <axu39@gatech.edu>
2026-02-11 11:27:18 -0500

    Adding training script

---
+++ stacked-hourglass/train.py
@@ -0,0 +1,85 @@
+from model.stackedhourglass import StackedHourGlass
+from data.lizarddataset import LizardDataset
+import numpy as np
+from torch import nn
+import torch
+from torch.utils.data import DataLoader
+import time
+from pathlib import Path
+from sklearn.model_selection import train_test_split
+import argparse
+
+def main(args):
+    training_data_dir = args.data
+    if training_data_dir is None:
+        training_data_dir = "./data/training_data"
+
+    npz_dir = Path(f"{training_data_dir}/heatmaps")
+    npz_paths = list(npz_dir.glob("*.npz"))
+    print(f"Found {len(npz_paths)} training samples")
+
+    train_paths, val_paths = train_test_split(npz_paths, test_size=0.2, random_state=42)
+
+    dataset = LizardDataset(train_paths, aug_factor=8)
+    dataloader = DataLoader(dataset, batch_size=8, shuffle=True, num_workers=0, pin_memory=True)
+
+    valid_dataset = LizardDataset(val_paths, aug_factor=1)
+    valid_dataloader = DataLoader(valid_dataset, batch_size=8, shuffle=False, num_workers=0, pin_memory=True)
+
+    device = 'cuda'
+    shg = StackedHourGlass()
+    shg.to(device)
+
+    optimizer = torch.optim.Adam(shg.parameters(), lr=1e-3)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        optimizer, mode='min', factor=0.5, patience=5
+    )
+    num_epochs = 100
+    for epoch in range(num_epochs):
+        shg.train()
+        running_loss = 0.0
+
+        total = len(dataloader.dataset)
+
+        batchct = 0
+        for imgs, gt_heatmaps in dataloader:
+            starttime = time.time()
+            imgs, gt_heatmaps = imgs.to(device), gt_heatmaps.to(device)
+            optimizer.zero_grad()
+            combined_hm_preds = shg(imgs)
+            loss = shg.calc_loss(combined_hm_preds, gt_heatmaps).mean()
+            loss.backward()
+            optimizer.step()
+            scheduler.step(avg_val_loss)
+            running_loss += loss.item()
+            batchct += 1
+            endtime = time.time()
+            runtime = endtime-starttime
+            print(f"Sample {batchct} / {total / 8} | Process Time: {runtime} s | ETA: {((total/8)-batchct)*runtime} | Loss: {loss.item()}", end="\r", flush=True)
+        avg_train_loss = running_loss / len(dataloader)
+
+        # -------------------------
+        # Validation
+        # -------------------------
+        shg.eval()
+        val_loss = 0.0
+        with torch.no_grad():
+            for imgs, gt_heatmaps in valid_dataloader:
+                imgs, gt_heatmaps = imgs.to(device), gt_heatmaps.to(device)
+                preds = shg(imgs)
+                loss = shg.calc_loss(preds, gt_heatmaps).mean()
+                val_loss += loss.item()
+        avg_val_loss = val_loss / len(valid_dataloader)
+        checkpoint_path = f"checkpoints/shg_epoch{epoch+1}.pth"
+        torch.save(shg.state_dict(), checkpoint_path)
+        print()
+        print(f"Epoch {epoch+1}/{num_epochs} | Train Loss: {avg_train_loss:.4f} | Val Loss: {avg_val_loss:.4f}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train Stacked Hourglass")
+
+    parser.add_argument("--data", type=str, required=False, help="Path to training data directory")
+
+    args = parser.parse_args()
+    main(args)
