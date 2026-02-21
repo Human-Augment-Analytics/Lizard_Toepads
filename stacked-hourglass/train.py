fee1da79625ad975158a176682fe58b3d008b523
Albert Xu <axu39@gatech.edu>
2026-02-21 10:59:51 -0500

    debugging loss

--- stacked-hourglass/train.py
+++ stacked-hourglass/train.py
@@ -64,6 +64,8 @@ def main(args):
             imgs, gt_heatmaps = imgs.to(device), gt_heatmaps.to(device)
             optimizer.zero_grad()
             combined_hm_preds = shg(imgs)
+            print(f"preds: {combined_hm_preds.shape}")
+            print(f"gt: {gt_heatmaps.shape}")
             loss = shg.calc_loss(combined_hm_preds, gt_heatmaps).mean()
             loss.backward()
             optimizer.step()
@@ -83,8 +85,6 @@ def main(args):
             for imgs, gt_heatmaps in valid_dataloader:
                 imgs, gt_heatmaps = imgs.to(device), gt_heatmaps.to(device)
                 preds = shg(imgs)
-                print("preds:", preds.shape)
-                print("gt   :", gt_heatmaps.shape)
                 loss = shg.calc_loss(preds, gt_heatmaps).mean()
                 val_loss += loss.item()
         avg_val_loss = val_loss / len(valid_dataloader)
